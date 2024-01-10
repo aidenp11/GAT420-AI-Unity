398910a108508d404f0be75a912f8d68e7fd5628
aidenp11 <129340716+aidenp11@users.noreply.github.com>
2024-01-10 14:24:31 -0700

    ai stuff

--- Assets/Scripts/AI DistancePerception.cs
+++ Assets/Scripts/AI DistancePerception.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class AIDistancePerception : MonoBehaviour
+public class AIDistancePerception : AIPerception
 {
-    // Start is called before the first frame update
-    void Start()
+    public override GameObject[] GetGameObjects()
     {
-        
-    }
+        List<GameObject> result = new List<GameObject>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Collider[] colliders = Physics.OverlapSphere(transform.position, distance);
+        foreach (Collider collider in colliders)
+        {
+            if (collider.gameObject == gameObject) continue;
+            if (tagName == "" || collider.CompareTag(tagName))
+            {
+                // calculate angle from transform forward vector to direction of game object
+                Vector3 direction = (collider.transform.position - transform.position).normalized;
+                float angle = Vector3.Angle(transform.forward, direction);
+                // if angle is less than max angle, add game object
+                if (angle <= maxAngle)
+                {
+                    result.Add(collider.gameObject);
+                }
+            }
+        }
+
+        return result.ToArray();
     }
 }
